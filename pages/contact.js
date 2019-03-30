abddcb89dd04db633f9506ed5bcffd437f08bd04
CatalystJesal <shadowii.jp@gmail.com>
2019-03-30 17:44:04 +0000

    added contact forms to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -14,7 +14,18 @@ export default () => (
             involving database or web development.
           </p>
           <br />
-          <p>Please fill out the form below:</p>
+          <p>Please contact me via the form below:</p>
+          <div className="form-container">
+            <form action="/send-email" method="post">
+              <label for="name">Name *</label>
+              <input type="text" placeholder="Your name" required />
+              <label for="email">Email *</label>
+              <input type="email" placeholder="Your email" required />
+              <label type="message">Message *</label>
+              <textarea name="message" placeholder="Your message" required />
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
         </div>
       </section>
     </div>
