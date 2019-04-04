115c06a1e9e2491a9d8d752101aa365c1686d70c
CatalystJesal <shadowii.jp@gmail.com>
2019-04-04 02:00:45 +0100

    emailing working for deployment via SendGrid

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:8000" : "website.jesal.now.sh";
+export const server = dev
+  ? "http://localhost:8000"
+  : "https://website.jesal.now.sh";
--- server/routes.js
+++ server/routes.js
@@ -1,17 +1,10 @@
 const server = require("./server");
 const Project = require("./project_schema");
 var connectToDatabase = require("./db");
-// const nodemailer = require("nodemailer");
-const aws = require("aws-sdk");
+const sgMail = require("@sendgrid/mail");
 
 //This file is the entry point for production mode
 require("dotenv").config();
-
-aws.config.update({
-  accessKeyId: process.env.SES_ACCESS,
-  secretAccessKey: process.env.SES_SECRET,
-  region: "eu-west-1"
-});
 
 try {
   server.get("/api/projects", (req, res) => {
@@ -26,41 +19,22 @@ try {
 }
 
 try {
-  server.post("/api/send-email", (req, res) => {
+  server.post("/api/send-email", async (req, res) => {
     const { name, email, message } = req.body;
 
     console.log(`${name} <${email}> ${message}`);
 
-    const ses = new aws.SES({
-      apiVersion: "latest"
-    });
+    sgMail.setApiKey(process.env.SENDGRID_API);
 
-    ses.sendEmail(
-      {
-        Source: "mail@jesal-patel.com",
-        Destination: {
-          ToAddresses: [process.env.EMAIL_ADDRESS]
-        },
-        Message: {
-          Subject: {
-            Data: `${name} <${email}>`
-          },
-          Body: {
-            Html: {
-              Data: message
-            }
-          }
-        }
-      },
-      function(err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Email sent:");
-          res.send(data);
-        }
-      }
-    );
+    const msg = {
+      to: process.env.EMAIL_ADDRESS,
+      from: "mail@jesal-patel.com",
+      subject: `${name} <${email}>`,
+      text: message
+    };
+
+    //It was all in the await call and making the function async! All this time!
+    await sgMail.send(msg);
 
     res.end("Message has been sent");
   });
@@ -69,50 +43,3 @@ try {
 }
 
 module.exports = server;
-
-// const auth = {
-//   api_user: "jesal24",
-//   api_key: process.env.SENDGRID_API
-// };
-
-// const transporter = nodemailer.createTransport(sgTransport(auth));
-
-// const auth = {
-//   type: "oauth2",
-//   user: process.env.EMAIL_ADDRESS,
-//   clientId: process.env.GOOGLE_CLIENT_ID,
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-//   accessToken:
-//     "ya29.GlvfBrSf9-gWJpG16zw09dAvkEyA8qmDl1UEN2L6V-S3_N5Atgqf0gNWp3TNmb7woJ1jFnJaAhDFD849UcPRLc9TS29GQ1ZLmERn-ivieX_zVinTMhFZyIE3LkdC"
-// };
-
-// const mailOptions = {
-//   from: process.env.EMAIL_ADDRESS,
-//   to: process.env.EMAIL_ADDRESS,
-//   subject: `${name} <${email}>`,
-//   text: message
-// };
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: auth,
-//   tls: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-// transporter.sendMail(mailOptions, (error, response) => {
-//   error ? console.log(error) : console.log(JSON.stringify(res));
-// });
-
-// sgMail.setApiKey(process.env.SENDGRID_API);
-
-// const msg = {
-//   to: process.env.EMAIL_ADDRESS,
-//   from: process.env.EMAIL_ADDRESS,
-//   subject: `${name} <${email}>`,
-//   text: message
-// };
-
-// sgMail.send(msg);
