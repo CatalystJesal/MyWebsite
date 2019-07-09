2090ea729882bee5d065f20cc5bd2c23106d0526
CatalystJesal <shadowii.jp@gmail.com>
2019-07-10 00:01:21 +0100

    DoB is dynamic

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,57 @@
 import React, { Component } from "react";
 import Layout from "../components/Layout";
 import Head from "next/head";
+import { AgeFromDate } from "age-calculator";
 
-export default () => (
-  <Layout>
-    <Head>
-      <title>Jesal Patel ｜ Home</title>
-    </Head>
-    <div className="container">
-      <section>
-        <div className="aboutme">
-          <h1>About me</h1>
-          <h2>
-            Name: Jesal Patel <br />
-            Age: 24 <br />
-            Location: London, UK
-          </h2>
-          <p>
-            I am a Software Developer graduated from City University London with
-            a Bachelor’s degree in Computer Science. Currently, I have one
-            year’s experience working as a back-end developer with technologies
-            such as C# and SQL.
-          </p>
-          <p>
-            My journey of becoming a developer started since my first year
-            studies at university. I really enjoyed digging into programming
-            problems, trying to find solutions and learning something new each
-            day. It was the same when I was working in my first job as a
-            developer after graduating. I may have finished my studies but I
-            remain a student as I try to learn new skills/technologies in my
-            spare time. Take this website as an example, learning to develop
-            skills in HTML5, CSS3, JavaScript, Next.js, React.js, MongoDB and
-            Node.js.
-          </p>
-          <p>
-            Outside of development my interests involve: volunteering for
-            charity, playing badminton, running, gaming and listening to music.
-          </p>
+export default class extends React.Component {
+  state = {
+    dateOfBirth: new AgeFromDate(new Date(1994, 6, 24)).age
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Head>
+          <title>Jesal Patel ｜ Home</title>
+        </Head>
+        <div className="container">
+          <section>
+            <div className="aboutme">
+              <h1>About me</h1>
+              <h2>
+                Name: Jesal Patel <br />
+                Age: {this.state.dateOfBirth} <br />
+                Location: London, UK
+              </h2>
+              <p>
+                I am a Software Developer graduated from City University London
+                with a Bachelor’s degree in Computer Science. Currently, I have
+                one year’s experience working as a back-end developer with
+                technologies such as C# and SQL.
+              </p>
+              <p>
+                My journey of becoming a developer started since my first year
+                studies at university. I really enjoyed digging into programming
+                problems, trying to find solutions and learning something new
+                each day. It was the same when I was working in my first job as
+                a developer after graduating. I may have finished my studies but
+                I remain a student as I try to learn new skills/technologies in
+                my spare time. Take this website as an example, learning to
+                develop skills in HTML5, CSS3, JavaScript, Next.js, React.js,
+                MongoDB and Node.js.
+              </p>
+              <p>
+                Outside of development my interests involve: volunteering for
+                charity, playing badminton, running, gaming and listening to
+                music.
+              </p>
+            </div>
+          </section>
         </div>
-      </section>
-    </div>
-  </Layout>
-);
+      </Layout>
+    );
+  }
+}
 
 // class App extends Component {
 //   state = {};
